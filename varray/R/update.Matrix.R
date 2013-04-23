555d502d96f2735c05fbbdc18609584d7f365672
tplate <tplate@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-04-23 01:52:03 +0000

    added update.matrix()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/abind@35 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- varray/R/update.Matrix.R
+++ varray/R/update.Matrix.R
@@ -26,3 +26,36 @@ update.Matrix <- function(x.name, data, need.dimnames=list(NULL, NULL)) {
     x[ii] <- as.vector(data)
     assign(x.name, value=x, pos=1)
 }
+
+update.matrix <- function(x.name, data, need.dimnames=list(NULL, NULL)) {
+    if (!exists(x.name)) {
+        if (!is.matrix(data))
+            stop('data must be a matrix')
+        x <- data
+    } else {
+        x <- get(x.name)
+        if (!is.matrix(x))
+            stop(x.name, ' already exists but is not a matrix')
+    }
+    dn <- list(sort(unique(c(rownames(x), rownames(data), need.dimnames[[1]])), na.last=NA),
+               sort(unique(c(colnames(x), colnames(data), need.dimnames[[2]])), na.last=NA))
+    sample <- replace(x[1,1], 1, 0L)
+    if (!isTRUE(all.equal(rownames(x), dn[[1]]))) {
+        new <- setdiff(dn[[1]], rownames(x))
+        if (length(new))
+            x <- rbind(x, array2(sample, dimnames=list(new, colnames(x))))
+        if (!isTRUE(all.equal(rownames(x), dn[[1]])))
+            x <- x[dn[[1]],,drop=FALSE]
+    }
+    if (!isTRUE(all.equal(colnames(x), dn[[2]]))) {
+        new <- setdiff(dn[[2]], colnames(x))
+        if (length(new))
+            x <- cbind(x, array2(sample, dimnames=list(rownames(x), new)))
+        if (!isTRUE(all.equal(rownames(x), dn[[1]])))
+            x <- x[dn[[1]],,drop=FALSE]
+    }
+    ii <- cbind(rep(match(rownames(data), rownames(x)), ncol(data)),
+                rep(match(colnames(data), colnames(x)), each=nrow(data)))
+    x[ii] <- as.vector(data)
+    assign(x.name, value=x, pos=1)
+}
